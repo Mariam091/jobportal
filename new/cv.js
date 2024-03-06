49cb2ef906c53d66843c9470d97d91db1941ef4a
Mariam091 <138693487+Mariam091@users.noreply.github.com>
2024-03-06 16:55:13 +0400

    Add files via upload

---
+++ new/cv.js
@@ -0,0 +1,3 @@
+document.getElementById("createCvButton").addEventListener("click", function() {
+        window.location.href = "http://localhost:63342/pythonProject/Frontend/CV/mycv.html?_ijt=3f0aet4a2s1pui6ir5e98inp35&_ij_reload=RELOAD_ON_SAVE;"
+    });
